942dc617157a97742540ccc6f52569bd8bdd859f
wangkaiwd <1162486176@qq.com>
2024-09-19 23:18:07 +0800

    feat: remove setName

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,22 +2,12 @@ import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { Button } from 'antd';
 import styles from './index.less';
 
 const HomePage = () => {
-  const { name, setName } = useModel('global');
+  const { name } = useModel('global');
   return (
     <PageContainer ghost>
-      <Button
-        onClick={() => {
-          setName(name + '1');
-          console.log(b);
-        }}
-        type={'primary'}
-      >
-        click
-      </Button>
       <div className={styles.container}>
         <Guide name={trim(name)} />
       </div>
