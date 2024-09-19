168cfeb3b81ea017a1c45a1d7c6ec2e86cd6441e
wangkaiwd <1162486176@qq.com>
2024-09-19 23:19:30 +0800

    feat: test push

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,6 +9,7 @@ const HomePage = () => {
   return (
     <PageContainer ghost>
       <div className={styles.container}>
+        test push
         <Guide name={trim(name)} />
       </div>
     </PageContainer>
